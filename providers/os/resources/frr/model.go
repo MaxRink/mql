acd9c1e7059d8150ef50b37026d906b68bc82fde
Maximilian Rink <maximilian.rink@telekom.de>
2026-09-19 11:54:24 +0200

    fix(frr): preserve VRF identities and accept compressed IPv6 peers
    Signed-off-by: Maximilian Rink <maximilian.rink@telekom.de>

--- providers/os/resources/frr/model.go
+++ providers/os/resources/frr/model.go
@@ -362,6 +362,7 @@ func (s *neighborSet) list() []Neighbor {
 	out := make([]Neighbor, 0, len(s.order))
 	for _, name := range s.order {
 		n := s.byName[name]
+		n.rollup()
 		sort.Strings(n.ActivatedAddressFamilies)
 		out = append(out, *n)
 	}
@@ -582,7 +583,6 @@ func applyNeighborAddressFamilyLine(set *neighborSet, afi, safi string, d *Direc
 	}
 
 	n.Params[strings.Join(rest, " ")] = boolParam(!d.Negated)
-	n.rollup()
 }
 
 // addressFamily returns the per-family settings of a neighbor, creating them
